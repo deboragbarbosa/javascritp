2fa4027afd69484fa630a79629f1d264e90637f9
Débora Barbosa <deboragbarbosa742@gmail.com>
2024-06-08 21:14:05 -0300

    ex14

--- Aula12ex/ex14/script.js
+++ Aula12ex/ex14/script.js
@@ -1,1 +1,23 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`;
+    if (hora >= 0 && hora <12){
+        //Bom Dia!
+        img.src= 'fotomanha.jpg';
+        document.body.style.background = '#e2cd9f'
+    }
+    else if (hora >= 12 && hora < 18 ) {
+        //Boa Tarde!
+        img.src = 'fototarde.jpg';
+        document.body.style.background = '#b98846f'
+    }
+    else { 
+        //Boa Noite!
+        img.src = 'fotonoite.jpg';
+        document.body.style.background ='#515154'
+    }
+
+}
