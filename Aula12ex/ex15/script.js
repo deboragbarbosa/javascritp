fa759bd8e413bca262f98c31bcc051ac4739f511
Débora Barbosa <deboragbarbosa742@gmail.com>
2024-06-18 00:44:59 -0300

    ex 015

--- Aula12ex/ex15/script.js
+++ Aula12ex/ex15/script.js
@@ -1,1 +1,28 @@
-alert('Olá')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.ariaValueMax.length == 0 || Number(fano.value)> ano) {window.alert('[ERRO] Verifique os dados e tente novamnete!')}
+    else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document. createElement('img')
+        img.setAttribute('id','foto')
+        if(fsex[0].checked){
+            gênero = 'Homem'
+            if(idade >=0 && idade < 10){
+                //Criança
+            } else if (idade <21){
+                //jovem 
+            } else if (idade < 50){
+                //Adulto
+            } else {
+                //Idoso
+            }
+
+        }
+    }
+    
+}
