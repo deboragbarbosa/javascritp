21aebe08b464aa51e445b7201a7df0b317466e2f
Débora Barbosa <deboragbarbosa742@gmail.com>
2024-06-24 20:00:03 -0300

    ex015

--- Aula12ex/ex15/script.js
+++ Aula12ex/ex15/script.js
@@ -3,20 +3,22 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.ariaValueMax.length == 0 || Number(fano.value)> ano) {window.alert('[ERRO] Verifique os dados e tente novamnete!')}
+    if (fano.value.length == 0 || fano.value> ano) {window.alert('[ERRO] Verifique os dados e tente novamente!')}
     else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var gênero = ''
+        var genero = ''
         var img = document. createElement('img')
         img.setAttribute('id','foto')
         if(fsex[0].checked){
-            gênero = 'Homem'
+            genero = 'Homem'
             if(idade >=0 && idade < 10){
                 //Criança
-                img.setAttribute('src','homembebecerto1.png')
+                img.setAttribute('scr','homem-bebe.png')
+        
             } else if (idade <21){
                 //jovem 
+                
             } else if (idade < 50){
                 //Adulto
             } else {
@@ -36,8 +38,9 @@ function verificar() {
                 //Idoso
             }
         }
-        res.computedStyleMap.texAling = 'center'
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos` 
+        res.appendChild(img)
     }
     
 }
