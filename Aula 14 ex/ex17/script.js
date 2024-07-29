6603e7a5384ca76eb412cc0c9463fa3e8aac24fe
Débora Barbosa <deboragbarbosa742@gmail.com>
2024-07-29 17:04:02 -0300

    ex017

---
+++ Aula 14 ex/ex17/script.js
@@ -0,0 +1,9 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (nun.value.length==0){
+        let n = Number (num.value)
+    }
+    
+}
