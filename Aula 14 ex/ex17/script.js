22aeb14e400be4ef80ace8958745551fd041cbb2
Débora Barbosa <deboragbarbosa742@gmail.com>
2024-07-30 00:20:35 -0300

    ex 017

--- Aula 14 ex/ex17/script.js
+++ Aula 14 ex/ex17/script.js
@@ -2,8 +2,20 @@ function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
 
-    if (nun.value.length==0){
-        let n = Number (num.value)
+    if ( num.value.length == 0) {
+        window.alert('Por favor, digite um numero!')
+    }
+    else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML= ''
+        while (c<= 10 ) {
+            let item = document.createElement('option')
+            item.text = `${n}x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
     
 }
